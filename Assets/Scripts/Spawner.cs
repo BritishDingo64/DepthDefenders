d1e3fe0b5a90bb414953446e18f503e4a3cc8cdb
George645 <gsvbutterfly@outlook.com>
2026-02-26 15:59:34 +0000

    forgot a few things

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawnerscript : MonoBehaviour {
+    //make an editor script that makes this run a line between the points.
+    List<GameObject> MonsterPath;
     void Start() {
 
     }
