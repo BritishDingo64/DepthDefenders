fae6547db7f9888519ef07ae629333116e8966dd
George645 <gsvbutterfly@outlook.com>
2026-02-26 15:58:36 +0000

    Starting work on the enemies
    https://docs.unity3d.com/6000.3/Documentation/ScriptReference/Editor.html

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Monster : MonoBehaviour {
+    void Start() {
+
+    }
+    void Update() {
+
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour {
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+    void SpawnMonster(Monster monster) {
+
+    }
+}
